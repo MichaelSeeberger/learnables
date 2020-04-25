b373022552604d9a7ce0509a69414a4f23f46f6e
Michael Seeberger <michael.seeberger@gmail.com>
2020-04-25 15:58:36 +0200

    Basic authorization

--- app/models/staff_profile.rb
+++ app/models/staff_profile.rb
@@ -1,4 +1,5 @@
 class StaffProfile < ApplicationRecord
+  resourcify
   has_one :user, dependent: :destroy
 
   accepts_nested_attributes_for :user
---
+++ app/policies/application_policy.rb
@@ -0,0 +1,50 @@
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    false
+  end
+
+  def show?
+    false
+  end
+
+  def create?
+    user.has_role?(:admin) or user.has_role?(:admin, record)
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    #false
+    create? or user.has_role?(:editor, record)
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope.all
+    end
+  end
+end
