08bd9b79dc10fc5de7bd2ea348d7cc9d154d25fa
Michael Seeberger <michael.seeberger@gmail.com>
2020-04-26 20:56:18 +0200

    enable test workers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,10 +8,7 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  #parallelize(workers: :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  #fixtures :all
+  parallelize(workers: :number_of_processors)
 
   include FactoryBot::Syntax::Methods
 
