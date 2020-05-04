a94785aa72d90d650a292f8f00ad860f0eec2f8c
Michael Seeberger <michael.seeberger@gmail.com>
2020-05-04 23:29:18 +0200

    redirect to sign in after sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_action :authenticate_user!
+
+  private
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
