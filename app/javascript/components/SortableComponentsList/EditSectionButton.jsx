81efb0109ef71e48cf6716924ed7f1855196a639
Michael Seeberger <michael.seeberger@gmail.com>
2020-05-05 17:24:08 +0200

    updates to section forms

--- app/javascript/components/SortableComponentsList/EditSectionButton.jsx
+++ app/javascript/components/SortableComponentsList/EditSectionButton.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 function EditSectionButton(props) {
     return (
-        <a className={"btn btn-sm btn-edit"} href={"/courses/"+props.section.course_id+"/sections/"+props.section.id+"/edit"}>Edit</a>
+        <span>
+            <a className={"btn btn-sm btn-edit"}
+           href={"/courses/" + props.section.course_id + "/sections/" + props.section.id + "/edit"}>Edit</a>
+            <a className={"btn btn-sm btn-delete ml-2"} data-confirm={'Are you sure?'}
+               href={"courses/" + props.course_id + "/sections/" + props.section.id}
+               data-method={"delete"}>Delete</a>
+        </span>
     )
 }
 
